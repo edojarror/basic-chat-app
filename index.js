4fa7120b042454b938e348ac42335765437cf739
Edo Adam Jarror <77976169+edojarror@users.noreply.github.com>
2023-05-03 18:33:12 +0700

    add ${user} is typing functionality

--- index.js
+++ index.js
@@ -31,19 +31,24 @@ io.on('connection', socket => {
     socket.on("user-login", (user) => {
         let checkAvaiablity = usersList.find(users => users.socketId === socket.id);
         if(checkAvaiablity === undefined) {
-            console.log(checkAvaiablity)
+            // console.log(checkAvaiablity)
             usersList.push({socketId: socket.id, username: user});
             socket.broadcast.emit("user_connect", `${user} was joined the chat room`);
             socket.emit("registeredUsername", user);
-            console.log(usersList)
+            // console.log(usersList)
         } else {
-            console.log("user list : ", usersList)
+            // console.log("user list : ", usersList)
             socket.emit("already-registered", "you already registered an username")
             socket.emit("dataAtServer", usersList)
         }
-        
-        
+    });
+
+    // broadcasting user is typing to all connected users
+    socket.on("userIsTypingText", text => {
+        // console.log(text)
+        socket.broadcast.emit("receiveUserTypingText", text)
     })
+    
     
 })
 
